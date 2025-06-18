1d12a737f93e39b06618b72fb440e5567486975f
omer galon <omergalon2004@gmail.com>
2025-06-18 18:15:17 +0300

    Starts on code simulations

---
+++ ANC LMS/ANC_Algo/Simulates_ANC.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+def get_noises_by_folder(folder:str):
+    pass
+
+
+
+
+
+def main():
+  folder_path=Path(r"C:\Users\Omer\Documents\Projects\WAVS\ANC WAVS\Noises")
+  wav_files=folder_path.glob("*.wav")
+
+
+
+if __name__=="__main__":
+    main()

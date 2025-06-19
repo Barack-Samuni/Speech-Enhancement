e7ce16ddf15f40a0cb2fd4e6d9b38f7b06792965
omerg12 <omergalon2004@gmail.com>
2025-06-19 14:21:40 +0300

    progress on running algs

--- ANC LMS/ANC_Algo/Simulates_ANC.py
+++ ANC LMS/ANC_Algo/Simulates_ANC.py
@@ -1,16 +1,64 @@
 from pathlib import Path
+import soundfile as sf
+import numpy as np
+from helping_methods_classes import SigArgs
 
 
-def get_noises_by_folder(folder:str):
-    pass
+def get_noises_by_folder(folder:Path):
+      """
+      *sf.read(wav_file) unpacks the tuple (noise_array, fs)
 
+      wav_file.stem gives the file name without extension
 
+      put in sig Args class and then do it to each noise file
+      """
+      if len(list(folder.glob("*wav"))) == 0:
+         raise FileNotFoundError(f"No .wav files found in the folder: {folder}")
+        
+      
+      return [SigArgs(*sf.read(n), n.stem) for n in folder.glob("*.wav")]   
+    
 
+def printfiles(wav_files: list):
+    """print all wav files in specified folder"""
+    for i, f in enumerate(wav_files):
+        print(f"[{i}] {f.name}")
+
+    print("\n")
+
+def get_one_sig(folder_sig:Path):
+    """returns the chosen cleaned signal to do on it ANC"""
+    wav_files=list(folder_sig.glob("*.wav"))
+    n=len(wav_files)
+    if n == 0:
+        print("No .wav files found in the folder.")
+        raise FileNotFoundError(f"No .wav files found in the folder: {folder_sig}")
+
+    printfiles(wav_files)
+    try:
+      index=int(input(f"Enter index of desired signal between 0 to {n-1}"))
+    except ValueError as e :
+        print("invalid value- must be integer")
+        raise e
+           
+    if not (0 <= index < n):
+      print("Index must be in the right limits")
+      raise IndexError
+        
+      
+    else:
+      sig, fs = sf.read(wav_files[index])
+      return SigArgs(sig, fs, wav_files[index].stem)
 
 
 def main():
-  folder_path=Path(r"C:\Users\Omer\Documents\Projects\WAVS\ANC WAVS\Noises")
-  wav_files=folder_path.glob("*.wav")
+  folder_path_noise=Path(r"C:\Users\Omer\Documents\Projects\WAVS\ANC WAVS\Noises")#USER INPUT OF NOISES FOLDER
+  folder_path_sig=Path(r"C:\Users\galon\Documents\projects\Wavs\Signals")#Giving the signal Folder
+
+  noise_list=get_noises_by_folder(folder=folder_path_noise)
+  sig=get_one_sig(folder_sig=folder_path_sig)
+   
+  
 
 
 
---
+++ ANC LMS/ANC_Algo/helping_methods_classes.py
@@ -0,0 +1,101 @@
+import numpy as np
+import soundfile as sf
+from pathlib import Path
+from scipy.signal import resample
+
+
+"""Sig arguenments helpful for output uses"""
+class SigArgs:
+    def __init__(self, noise, fs, name):
+        self._sig_array = noise
+        self._fs = fs
+        self._sig_name = name
+
+    @property
+    def sig_array(self):
+        return self._sig_array
+
+    @sig_array.setter
+    def sig_array(self, value):
+        self._sig_array = value
+
+    @property
+    def fs(self):
+        return self._fs
+
+    @fs.setter
+    def fs(self, value):
+        self._fs = value
+
+    @property
+    def name(self):
+        return self._sig_name
+
+    @name.setter
+    def name(self, value):
+        self._sig_name = value
+
+def resample_fs(sig,fs_old,fs_new):
+    num_samples = int(len(sig) * fs_new / fs_old)
+    resampled_sig = resample(sig, num_samples)
+    return resampled_sig,fs_new
+
+def common_length(sig1,sig2):
+    min_len = min(len(sig1), len(sig2))
+    return min_len
+
+def create_folder(path: Path, folder_name: str):
+    """
+    path / folder_name: creates a Path object for the new folder.
+
+    mkdir(parents=True, exist_ok=True):
+
+    parents=True: creates intermediate folders if they don’t exist.
+
+    exist_ok=True: avoids error if the folder already exists.
+
+    """
+    new_folder = path / folder_name
+    new_folder.mkdir(parents=True, exist_ok=True)
+    return new_folder
+    
+def generate_full_sig(sig_object:SigArgs,noise_object:SigArgs,noise_lin_amp:float=3.0):
+    noise=noise_object._sig_array
+    fs_noise=noise_object.fs
+
+    sig=sig_object._sig_array
+    fs_sig=sig_object.fs
+    
+    if(noise.ndim>=1):
+        noise = noise.mean(axis=1)
+        noise_object.sig_array=noise
+    if(sig.ndim>=1):    
+        sig=sig.mean(axis=1)
+        sig_object.sig_array=sig
+
+    if fs_sig>fs_noise:
+        sig,fs1=resample_fs(sig,fs_old=fs1,fs_new=fs2)
+        sig_object.sig_array=sig
+        sig_object.fs=fs1
+
+    elif fs_sig<fs_noise:
+        noise,fs2=resample_fs(sig,fs_old=fs2,fs_new=fs1)
+        noise_object.sig_array=noise
+        noise_object.fs=fs2
+
+        
+    coomon_len=common_length(sig,noise)
+    sig=sig[:coomon_len]
+    noise=noise[:coomon_len]*noise_lin_amp
+    final_sig=sig+noise
+    sig_object.sig_array=sig
+    noise_object.sig_array=noise
+
+
+    file_add=r"C:\Users\galon\Documents\projects\Wavs\exp1"
+    sf.write(file_add+r"\full_sig_noise_sig2.wav",final_sig, fs1)
+    sf.write(file_add+r"\exp_sig2.wav",sig,fs1)
+    sf.write(file_add+r"\exp_noise2.wav",noise,fs1)
+   
+
+
